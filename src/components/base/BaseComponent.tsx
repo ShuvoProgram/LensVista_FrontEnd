befce0f1ba3b5d1a837841bf06cd45898064d74c
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-27 01:21:50 +0600

    Refactor: Optimize BaseComponent render 🛠️ Update: Navbar and Footer conditional rendering 🔄 Feature: Integrate CustomProvider for context 🚀 Chore: Improve code readability in BaseComponent 📚 Fix: Ensure consistent className usage in BaseComponent 🩹

---
+++ src/components/base/BaseComponent.tsx
@@ -0,0 +1,29 @@
+import React, { Suspense } from "react";
+import Navbar from "../ui/Navbar/Navbar";
+import Footer from "../ui/Footer/Footer";
+import CustomProvider from "@/providers/CustomProvider";
+import { Toaster } from "../ui/toaster";
+import InitializeUser from "./initializeUser";
+
+export default async function BaseComponent({
+  children,
+  showNavbar = true,
+  showFooter = true,
+  className = "w-[95%] mx-auto",
+}: {
+  children: React.ReactNode;
+  showNavbar?: boolean;
+  showFooter?: boolean;
+  className?: string;
+}) {
+  return (
+    <CustomProvider>
+      <Toaster />
+      <div className={className}>
+        {showNavbar && <Navbar />}
+        {children}
+        {showFooter && <Footer />}
+      </div>
+    </CustomProvider>
+  );
+}
