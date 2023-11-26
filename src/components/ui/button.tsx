1a69e828feecb4de823eda8297b0970754c8515b
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-27 02:54:36 +0600

    🔄 Updated Button component configurations for enhanced usability and versatility. 🎨 Tweaked Button styling and variants, refining visual consistency and responsiveness. 🚀 Optimized Button functionality, fine-tuning sizes and variants for improved UI adaptability. 🛠️ Refined Button component attributes, ensuring seamless integration and usage. 🌟 Enhanced Button configurations, polishing styles and sizes for a refined user experience.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils"
 
@@ -51,6 +51,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   }
 )
-Button.displayName = "Button"
+Button.displayName = "Button";
 
 export { Button, buttonVariants }
