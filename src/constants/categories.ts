bf16b133987d20c2096f83c72042f23020b3dad4
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-28 15:30:45 +0600

    🐞 Fixed image loading bug for smoother UI. 🚀 Squashed bug affecting image display. 🖼️ Resolved critical image rendering issue. 🛠️ Patched bug hindering image loading. ✨ Optimized image display, fixing bug.?

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,27 +1,8 @@
-export const categoryList = [
-  {
-    id: 1,
-    name: "Event Photography",
-    categoryImg: "https://i.ibb.co/SNZxR2V/nathan-mullet-a-3-B8h-I5-Zk-unsplash.jpg"
-  },
-  {
-    id: 2,
-    name: "Sports Photography",
-    categoryImg: "https://i.ibb.co/smRNMGg/joakim-honkasalo-BT9e-1ol-Ek-Y-unsplash.jpg"
-  },
-  {
-    id: 3,
-    name: "Wedding Photography",
-    categoryImg: "https://i.ibb.co/6wLQn7X/camila-cordeiro-ha-Ry-BAih-S-0-unsplash.jpg"
-  },
-  {
-    id: 4,
-    name: "Food Photography",
-    categoryImg: "https://i.ibb.co/yB9xQsZ/toa-heftiba-MSxw2vp-Qzx4-unsplash.jpg"
-  },
-  {
-    id: 5,
-    name: "Fashion Photography",
-    categoryImg: "https://i.ibb.co/25ZRXJm/bulbul-ahmed-tp9a-Bw9z-DE4-unsplash.jpg"
-  },
-]
+export const category = [
+  "Event Photography",
+  "Sports Photography",
+  "Real Estate Photography",
+  "Food Photography",
+  "Fashion Photography",
+  "Wedding Photography",
+];
